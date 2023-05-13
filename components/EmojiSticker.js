3a3a27fa44146b017fb178eb55cda4941ea841d7
Gregg Wong <gwong@smartterm.io>
2023-05-12 21:05:10 -0500

    Expand component with Animated lib

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -1,16 +1,71 @@
 import { StyleSheet, Text, View } from 'react-native'
+import Animated, {
+    useAnimatedStyle,
+    useSharedValue,
+    useAnimatedGestureHandler,
+    withSpring
+} from 'react-native-reanimated'
 import React from 'react'
+import { PanGestureHandler, TapGestureHandler } from 'react-native-gesture-handler';
 
-const EmojiSticker = ({imageSize, stickerSource}) => {
-  return (
-    <View style={{ top: -350 }}>
-      <Image 
-        source={stickerSource}
-        resizeMode="contain"
-        style={{ width: imageSize, height: imageSize }}
-        />
-    </View>
-  )
+// This Animated library works by looking at the style props
+// of the component and determines which value is animated or 
+// is to be animated.
+const AnimatedImage = Animated.createAnimatedComponent(Image);
+const AnimatedView = Animated.createAnimatedComponent(View);
+
+const EmojiSticker = ({ imageSize, stickerSource }) => {
+    const translateX = useSharedValue(0);
+    const translateY = useSharedValue(0);
+
+    const containerStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                { translateX: translateX.value },
+                { translateY: translateY.value }
+            ]
+        }
+    });
+
+    const scaleImage = useSharedValue(imageSize);
+    const onDoubleTap = useAnimatedGestureHandler({
+        onActive: () => {
+            if (scaleImage.value) {
+                scaleImage.value = imageSize * 2;
+            }
+        }
+    });
+    const imageStyle = useAnimatedStyle(() => {
+        return {
+            width: withSpring(scaleImage.value),
+            height: withSpring(scaleImage.value)
+        }
+    });
+
+    const onDrag = useAnimatedGestureHandler({
+        onStart: (event, context) => {
+            context.translateX = translateX.value;
+            context.translateY = translateY.value;
+        },
+        onActive: (event, context) => {
+            translateX.value = context.translateX + event.translationX;
+            translateY.value = context.translateY + event.translationY;
+        }
+    }
+    );
+    return (
+        <PanGestureHandler onGestureEvent={onDrag}>
+            <AnimatedView style={[containerStyle,{ top: -350 }]}>
+                <TapGestureHandler onGestureEvent={onDoubleTap} numberOfTaps={2}>
+                    <AnimatedImage
+                        source={stickerSource}
+                        resizeMode="contain"
+                        style={[imageStyle, { width: imageSize, height: imageSize }]}
+                    />
+                </TapGestureHandler>
+            </AnimatedView>
+        </PanGestureHandler>
+    )
 }
 
 export default EmojiSticker
